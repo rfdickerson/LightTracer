e875ad0d1df56e3353b3d5ebd040c3867d1fb9dd
Robert F. Dickerson <rfdickerson@us.ibm.com>
2016-06-23 11:24:12 -0500

    Material, Camera, Sphere as different files

--- Sources/PathTracer/Material.swift
+++ Sources/PathTracer/Material.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+
+public struct Material {
+    
+    /// emission color, typically if a light
+    let emission: Color
+    
+    let diffuseColor: Color
+    
+    /// specular weight
+    let ks: Float
+    
+    /// diffuse weight
+    let kd: Float
+    
+    /// specular exponent
+    let n: Float
+    
+    public init(emission: Color, diffuseColor: Color,
+                ks: Float, kd: Float, n: Float) {
+        
+        self.emission = emission
+        self.diffuseColor = diffuseColor
+        self.ks = ks
+        self.kd = kd
+        self.n = n
+        
+    }
+    
+    
+}
--- Sources/PathTracer/Sphere.swift
+++ Sources/PathTracer/Sphere.swift
@@ -7,3 +7,55 @@
 //
 
 import Foundation
+
+public struct Sphere {
+    let center: Vector3D
+    let radius: Float
+    let radiusSquared: Float
+    let material: Material
+    
+    public init(center: Vector3D, radius: Float, material: Material) {
+        self.radiusSquared = radius*radius
+        self.center = center
+        self.radius = radius
+        self.material = material
+    }
+}
+
+extension Sphere : Intersectable {
+    
+    /**
+     Ray-sphere intersection test
+     
+     - parameter origin: ray origin
+     - parameter direction: ray direction
+     - returns: distance from the ray origin to the intersection point
+     */
+    public func intersect(origin: Vector3D, direction: Vector3D) -> Float? {
+        
+        // transform ray to object space
+        // let transform = Matrix44.createTransform(withTranslation: Vector3D(x: -center.x, y: -center.y, z: -center.z))
+        
+        // let L = transform * origin
+        let L = origin - center
+        
+        let a = dot(direction, direction)
+        let b = 2*dot(direction, L)
+        let c = dot(L, L) - radiusSquared
+        
+        guard let roots = solveQuadratic(a: a, b: b, c: c) else {
+            return nil
+        }
+        
+        let t0 = min(roots.0, roots.1)
+        let t1 = max(roots.0, roots.1)
+        
+        if t0<0 && t1<0 {
+            return nil
+        }
+        
+        return t0
+        
+    }
+    
+}
